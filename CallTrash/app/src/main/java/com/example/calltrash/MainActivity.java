9023cf3fc261dc238de160ddcdbaac9d67aa2e42
Hanif <hanifsatrio12@gmail.com>
2021-07-07 12:12:18 +0700

    Login-Register

---
+++ CallTrash/app/src/main/java/com/example/calltrash/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.calltrash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_register = findViewById(R.id.btn_register);
+        Button btn_login = findViewById(R.id.btn_login);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity((new Intent(MainActivity.this, HomeActivity.class)));
+            }
+        });
+    }
+}
